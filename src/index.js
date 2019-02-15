fdbb3959887741946f113377ff9184c7a1b80792
databank <adrian@databank.ro>
2019-02-15 13:40:27 +0200

    src/

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+var Cloudwatch = function( config ) {
+	
+}
+
+Cloudwatch.prototype.putMetricData = function( params, cb ) {
+	cb()
+}
+Cloudwatch.prototype.getMetricStatistics = function( params, cb ) {
+	cb()
+}
+
+
+module.exports = Cloudwatch;
