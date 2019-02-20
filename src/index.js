3ed729c7a3536be49b213711f99cb29eb40f47bc
databank <adrian@databank.ro>
2019-02-20 14:20:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -71,14 +71,108 @@ Cloudwatch.prototype.putMetricData = function( params, cb ) {
 	}, function(err) {
 		cb(err)
 	})
-
-
-
-	
 }
-Cloudwatch.prototype.getMetricStatistics = function( params, cb ) {
+
+
+/*
+var params = {
+	StartTime: new Date( parseInt(event._GET.start) ),
+	EndTime:   new Date( parseInt(event._GET.end)   ),
+
+	MetricName: event._GET.metric,
+	Namespace: event._GET.namespace,
+	Period: parseInt( event._GET.period ), // agregate at 1 day
+	Statistics: [ event._GET.statistics  ],  // SampleCount | Average | Sum | Minimum | Maximum,
+};
+
+if (event._GET.dimension_name) {
+	params.Dimensions = [{
+		Name:  event._GET.dimension_name, 
+		Value: event._GET.dimension_value,
+	}]
+}
+if (event._GET.unit)
+	params.Unit = event._GET.unit;
+
+cloudwatch.getMetricStatistics(params, function(err, data) {
+
+
+	data.Datapoints.map(function(dp) {
+		var newdate = new Date(
+			new Date( dp.Timestamp ).getTime()
+		).toISOString()
+
+		cwseries[ newdate.slice(0, date_slice ) ] += dp[event._GET.statistics];
+	})
+
+
 	cb()
+});
+*/
+
+Cloudwatch.prototype.getMetricStatistics = function( params, cb ) {
+
+	var $this=this;
+	var between_start;
+	var between_end;
+
+	switch (params.Period ) {
+		case 60*60*24: // 24h
+			between_start = 'H ' + (new Date(params.StartTime).toISOString().slice(0,13).split('T').join(' ')  )
+			between_end   = 'H ' + (new Date(params.EndTime  ).toISOString().slice(0,13).split('T').join(' ')  )
+			break;
+		case 60*60: // 1h
+			between_start = 'M ' + (new Date(params.StartTime).toISOString().slice(0,16).split('T').join(' ')  )
+			between_end   = 'M ' + (new Date(params.EndTime  ).toISOString().slice(0,16).split('T').join(' ')  )
+			break;
+
+	}
+	
+	if (!between_start)
+		return cb({ errorCode: 'invalid period'})
+
+	console.log(between_start)
+	console.log(between_end)
+	
+	
+	console.log(params)
+
+	DynamoDB
+		.table( $this.config.table_name || process.env.CW_DYNAMODB_TABLE)
+		.where('namespace').eq(params.Namespace)
+		.where('date').between( between_start, between_end )
+		.query(function(err, data) {
+			
+			var ret = {
+				// Label:
+				Datapoints: [], // { Timestamp: Sum:  }
+			}
+			var Datapoints = {}
+			data.map(function(d) {
+				switch (params.Period ) {
+					case 60*60: // 1h
+						var date = new Date((d.date.slice(2,15) + ':00:00.000Z').split(' ').join('T')).getTime()
+						if (!Datapoints.hasOwnProperty(date))
+							Datapoints[date] = 0
+
+						Datapoints[date] += d[ params.MetricName ];
+						break;
+				}
+			})
+
+			console.log('Datapoints', Datapoints )
+			Object.keys(Datapoints).map(function(timestamp) {
+				ret.Datapoints.push({
+					Timestamp: timestamp,
+					Sum: Datapoints[timestamp],
+				})
+			})
+			//console.log( err, data )
+			cb(null, ret )
+		})
+
 }
+
 
 
 module.exports = Cloudwatch;
--- test/tests/002.js
+++ test/tests/002.js
@@ -17,24 +17,29 @@ describe('getMetricStatistics', function () {
 		});
 	})
 
-	// it('getMetricStatistics', function(done) {
-	// 
-	// 	var params = {
-	// 		Namespace: 'hello/world',
-	// 
-	// 		StartTime: new Date( '2019-01-18T00:00:00.000Z' ),
-	// 		EndTime:   new Date( '2019-02-20T23:59:00.000Z' ),
-	// 
-	// 		MetricName: 'sent',
-	// 
-	// 		Period: 60*60*24, // in seconds 
-	// 		Statistics: [ 'Sum' ],  // SampleCount | Average | Sum | Minimum | Maximum,
-	// 	};
-	// 
-	// 	cloudwatch.getMetricStatistics( params, function( err, data ) {
-	// 		console.log(err ? '☐' : '☑', "getMetricStatistics.sent", err )
-	// 		done()
-	// 	})
-	// })
+	it('sleep', function(done) {
+		setTimeout(done, 1000*20)
+	})
+
+	it('getMetricStatistics', function(done) {
+	
+		var params = {
+			Namespace: 'hello/world/1',
+	
+			StartTime: new Date( '2019-01-18T00:00:00.000Z' ),
+			EndTime:   new Date( '2019-02-20T23:59:00.000Z' ),
+	
+			MetricName: 'sent',
+	
+			Period: 60*60, // in seconds 
+			Statistics: [ 'Sum' ],  // SampleCount | Average | Sum | Minimum | Maximum,
+		};
+	
+		cloudwatch.getMetricStatistics( params, function( err, data ) {
+			console.log(err ? '☐' : '☑', "getMetricStatistics.sent", err )
+			console.log(data)
+			done()
+		})
+	})
 
 })
