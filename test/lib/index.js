d30a9d45d17ae9db331cd7cb20d96f6607143c8c
databank <adrian@databank.ro>
2019-02-20 13:34:13 +0200

    update

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,3 +1,4 @@
+async = require('async')
 AWS = require('aws-sdk')
 
 // cloudwatch dynamodb
@@ -5,7 +6,7 @@ process.env.CW_DYNAMODB_ENDPOINT="http://localhost:8000"
 process.env.CW_DYNAMODB_KEY="myKeyId"
 process.env.CW_DYNAMODB_SECRET="secretKey"
 process.env.CW_DYNAMODB_REGION="us-east-1"
-
+process.env.CW_DYNAMODB_TABLE ="stats"
 
 Cloudwatch = require("../../src/index")
 
